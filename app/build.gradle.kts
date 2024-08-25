203bfffbb36f538e15efd56622d5d3b8bb8238c4
Matheus <matheusvalbert@gmail.com>
2024-08-24 22:35:11 -0300

    update dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
     applicationId = "com.matheusvalbert.programmercalculator"
     minSdk = 28
     targetSdk = 34
-    versionCode = 5
-    versionName = "1.1.3"
+    versionCode = 6
+    versionName = "1.2.0"
 
     multiDexEnabled = true
 
@@ -66,12 +66,11 @@ android {
 }
 
 dependencies {
-
   implementation(platform("com.google.firebase:firebase-bom:32.8.1"))
   implementation("androidx.core:core-ktx:1.13.1")
-  implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
-  implementation("androidx.activity:activity-compose:1.9.0")
-  implementation(platform("androidx.compose:compose-bom:2024.05.00"))
+  implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.4")
+  implementation("androidx.activity:activity-compose:1.9.1")
+  implementation(platform("androidx.compose:compose-bom:2024.08.00"))
   implementation("androidx.compose.ui:ui")
   implementation("androidx.compose.ui:ui-graphics")
   implementation("androidx.compose.ui:ui-tooling-preview")
@@ -79,7 +78,7 @@ dependencies {
   implementation("com.google.accompanist:accompanist-systemuicontroller:0.27.0")
   implementation("com.google.dagger:hilt-android:2.44.2")
   kapt("com.google.dagger:hilt-android-compiler:2.44.2")
-  implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.7.0")
+  implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.4")
   kapt("androidx.hilt:hilt-compiler:1.2.0")
   implementation("org.springframework:spring-expression:6.0.11")
   implementation("com.google.firebase:firebase-analytics-ktx")
@@ -87,9 +86,9 @@ dependencies {
   implementation("androidx.datastore:datastore-preferences-core:1.1.1")
   implementation("com.google.android.play:review-ktx:2.0.1")
   testImplementation("junit:junit:4.13.2")
-  androidTestImplementation("androidx.test.ext:junit:1.1.5")
-  androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-  androidTestImplementation(platform("androidx.compose:compose-bom:2024.05.00"))
+  androidTestImplementation("androidx.test.ext:junit:1.2.1")
+  androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
+  androidTestImplementation(platform("androidx.compose:compose-bom:2024.08.00"))
   androidTestImplementation("androidx.compose.ui:ui-test-junit4")
   debugImplementation("androidx.compose.ui:ui-tooling")
   debugImplementation("androidx.compose.ui:ui-test-manifest")
